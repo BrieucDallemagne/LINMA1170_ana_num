e8e48e0e46ecb3119aeb1e1c429be33faf21a6c4
Brieuc Dallemagne <brieuc1160@gmail.com>
2024-05-17 09:24:10 +0200

    img

--- Devoir4/compression_image.py
+++ Devoir4/compression_image.py
@@ -12,15 +12,23 @@ blue, green, red = cv2.split(img)
 def compress_channel(channel):
     U, S, Vt = np.linalg.svd(channel, full_matrices=False)
     k = 50  
-    S1 = np.zeros_like(S)
+    S1 = np.zeros(k)
     S1[:k] = S[:k]
-    compressed_channel = np.dot(U, np.dot(np.diag(S1), Vt))
-    return compressed_channel
+    compressed_U = U[:, :k]
+    compressed_Vt = Vt[:k, :]
+    #wite those 3 matrix in a data.txt file
+    np.savetxt('data.txt', compressed_U, fmt='%f')
+    np.savetxt('data.txt', compressed_Vt, fmt='%f')
+    np.savetxt('data.txt', S1, fmt='%f')
+    #compressed_channel = np.dot(compressed_U, np.dot(np.diag(S1), compressed_Vt))
+    #return compressed_channel
+
+
 
 compressed_blue = compress_channel(blue)
 compressed_green = compress_channel(green)
 compressed_red = compress_channel(red)
 
-compressed_image = cv2.merge([compressed_blue, compressed_green, compressed_red])
+#compressed_image = cv2.merge([compressed_blue, compressed_green, compressed_red])
 
-cv2.imwrite('outputs/compressed_image.jpg', compressed_image)
+#cv2.imwrite('outputs/compressed_image.jpg', compressed_image)
